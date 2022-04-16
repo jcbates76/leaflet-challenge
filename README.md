6561234c965be22bbc639bb54c75aa6e7b9409df
Jason Bates <94392882+jcbates76@users.noreply.github.com>
2022-04-16 13:03:37 -0500

    program done and markdown complete

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # leaflet-challenge
 Project utilizing leaflet
+
+## Basic Visualization
+This project pulls recent earthquake data from the USGS and creates a visualization of that data.  
+
+The dataset utilized was the recent 30-day earthquake activity.  
+Website = https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson  
+
+### Markers
+The latitude and longitude of the epicenter of the earthquake was used as the location of the marker.  
+The marker size is based on the magnitude of the earthquake - with larger magnitudes having larger diameter markers.  
+The marker color is based on the depth of the earthquake (the third coordinate provided).  
+Popups (when a marker is clicked) will show the basic information of the earthquake including magnitude, location name, and depth.  
+
+### Map
+The map is initialized on the webpage.  
+A legend is created based on the scale utilized to visualize the depth of the earthquake.  
--- static/js/logic.js
+++ static/js/logic.js
@@ -11,26 +11,64 @@ let myMap = L.map("map", {
   }).addTo(myMap);
   
 // Define the link for the GeoJSON data
-  const link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson"
+  const link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
 // Get the GeoJSON data
 d3.json(link).then((data) => {
-    // Verify the data is pulled by showing on the console  
-    console.log(data);
-    console.log(data['features'])
-    for (let i = 1; i < data['features'].length; i++) {
-        console.log(data.features[i])
+    // Loop through the data to retrieve the marker data
+    for (let i = 0; i < data['features'].length; i++) {
+        // Pull the latitude and longitude for the earthquake location for marker locations
         let lat = data['features'][i]['geometry']['coordinates'][1];
         let lng = data['features'][i]['geometry']['coordinates'][0];
+        // Pull the depth for the earthquake for the marker color
         let depth = data['features'][i]['geometry']['coordinates'][2];
+        // Pull the magnitude of the earthquake for the marker size
         let mag = data['features'][i]['properties']['mag']
-        let magradius = 50000 * mag
+        let magradius = Math.pow(8, mag);
+        // Pull the info data for the popup
+        let info = data['features'][i]['properties']['title'];
+        // Create the marker including the popup
         L.circle ([lat, lng], {
             weight: 0.25,
             color: "black",
-            fillColor: "pink",
+            fillColor: color(depth),
             fillOpacity: 0.50,
             radius: magradius
-        }).addTo(myMap);
+        }).bindPopup(`<h3>${info}; Depth: ${depth}</h3>`).addTo(myMap);
     };
 });
+
+// Function to select a color for the marker based on the depth of the earthquake
+function color(zcoord) {
+    if (zcoord <= 20) {
+        depthcolor = "#333ED4";
+    } else if (zcoord <= 40) {
+        depthcolor = "#2FA236"
+    } else if (zcoord <= 60) {
+        depthcolor = "#A0D636"
+    } else if (zcoord <= 80) {
+        depthcolor = "#EEDE04" 
+    } else if (zcoord <= 100) {
+        depthcolor = "#F76915"
+    } else {
+        depthcolor = "#FD0100"
+    };
+    return depthcolor;
+};
+
+// Create a legened for the map
+let legend = L.control({
+    position: "bottomright"
+});
+legend.onAdd = function () {
+    let div = L.DomUtil.create('div', 'legend');
+    earthquakeDepth = [0, 20, 40, 60, 80, 100];
+    colors = ["#333ED4", "#2FA236", "#A0D636", "#EEDE04", "#F76915", "#FD0100"];
+    for (let i = 0; i < earthquakeDepth.length; i++) {
+            div.innerHTML +=
+                "<i style='background: " + colors[i] + "'></i>" +
+                earthquakeDepth[i] + (earthquakeDepth[i+1] ? "-" + earthquakeDepth[i+1] + "<br>": "+");
+    }
+    return div;
+}
+legend.addTo(myMap);
